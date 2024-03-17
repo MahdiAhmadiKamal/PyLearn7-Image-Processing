1c5603b877af8ce9b93728436f4d83d55751ccef
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-03-17 13:48:56 +0330

    6th commit

---
+++ PyLearn7-Assignment26/5) gradient/gradient.py
@@ -0,0 +1,15 @@
+import cv2
+
+
+board = cv2.imread("square-512.jpg")
+color = 255
+
+for row in range(board.shape[0]):
+    color -= 255/board.shape[0]
+    for col in range(board.shape[1]):
+        board[row][col] = color
+    
+cv2.imshow("", board)
+# cv2.imshow("title",my_image_2)
+cv2.waitKey()
+cv2.imwrite("result.jpg", board)
