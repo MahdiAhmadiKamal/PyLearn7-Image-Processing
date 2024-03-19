a3620b8804ce829182a56e311e4376d5198d859a
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-03-19 21:09:53 +0330

    2nd commit

--- PyLearn7-Assignment27/1) logo/logo.py
+++ PyLearn7-Assignment27/1) logo/logo.py
@@ -1,7 +1,7 @@
 import cv2
 
 
-image = cv2.imread("pics\image.jpg")
+image = cv2.imread("image.jpg")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # print(image)
 
---
+++ PyLearn7-Assignment27/2) tv noise/noise_2.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+theme_image = cv2.imread("image2.png")
+theme_image = cv2.cvtColor(theme_image, cv2.COLOR_BGR2GRAY)
+
+rows = theme_image.shape[0]
+cols = theme_image.shape[1]
+
+writer = cv2.VideoWriter("output_video.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 30, (cols, rows))
+
+while True:
+    noise_image = np.random.random((134, 214)) * 255
+    noise_image = np.array(noise_image, dtype=np.uint8)
+
+    theme_image[156:290, 238:452] = noise_image
+    writer.write(theme_image)
+    cv2.imshow("noise", theme_image)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+writer.release()
