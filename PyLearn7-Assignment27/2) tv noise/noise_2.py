e73178f38c743e13eed94e789b0a1593c813d907
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-03-20 12:39:42 +0330

    3rd commit

--- PyLearn7-Assignment27/2) tv noise/noise_2.py
+++ PyLearn7-Assignment27/2) tv noise/noise_2.py
@@ -1,22 +1,30 @@
 import cv2
 import numpy as np
+import imageio
 
-theme_image = cv2.imread("image2.png")
-theme_image = cv2.cvtColor(theme_image, cv2.COLOR_BGR2GRAY)
 
-rows = theme_image.shape[0]
-cols = theme_image.shape[1]
+image = cv2.imread("image2.png")
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-writer = cv2.VideoWriter("output_video.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 30, (cols, rows))
+rows = image.shape[0]
+cols = image.shape[1]
 
+writer = cv2.VideoWriter("output_video2.mp4", cv2.VideoWriter_fourcc(*'XVID'), 30, (cols, rows))
+
+frames = []
 while True:
     noise_image = np.random.random((134, 214)) * 255
     noise_image = np.array(noise_image, dtype=np.uint8)
 
-    theme_image[156:290, 238:452] = noise_image
-    writer.write(theme_image)
-    cv2.imshow("noise", theme_image)
+    image[156:290, 238:452] = noise_image
+
+    new_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    frames.append(new_image)
+    
+    writer.write(new_image) 
+    cv2.imshow("noise", new_image)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
+imageio.mimsave ("output_gif2.gif", frames)
 writer.release()
