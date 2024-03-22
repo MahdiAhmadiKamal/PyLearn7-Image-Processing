d52a0f7983f381ccd8fce76b71655bf4c0468732
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-03-22 18:07:00 +0430

    7th commit

--- PyLearn7-Assignment27/4) color detector/color_detector.py
+++ PyLearn7-Assignment27/4) color detector/color_detector.py
@@ -2,6 +2,7 @@ import fractions
 import cv2
 import numpy as np
 
+
 cap = cv2.VideoCapture(0)
 box = np.zeros((110, 110), dtype=int)
 
@@ -14,22 +15,31 @@ while True:
     box_col_2 = frame.shape[1]//2 + 55
     box_row_2 = frame.shape[0]//2 + 55
     
-    cv2.rectangle(frame, (box_col_1, box_row_1), (box_col_2, box_row_2), 0, 10)
-    box[0:110, 0:110] = frame[box_col_1:box_col_2, box_row_1:box_row_2]
+    cv2.rectangle(frame, (box_col_1, box_row_1), (box_col_2, box_row_2), 0, 7)
+    box = frame[box_col_1:box_col_2, box_row_1:box_row_2]
 
-    # for row in range(box_row_1, box_row_2):
-    #     for col in range(box_col_1, box_col_2):
-    #         c = np.count_nonzero(frame < 100)
-    #         if c > 2000:
-    #             print("black")
+    b = np.count_nonzero((box < 80))
+    g = np.count_nonzero((box > 120) & (box < 160))
+    w = np.count_nonzero((box > 150))
+    
+    if b > 0.7 * (box.shape[0] * box.shape[1]):
+        # print("black")
+        color = "[Black]"
+    elif g > 0.7 * (box.shape[0] * box.shape[1]):
+        color = "[Gray]"
+    elif w > 0.7 * (box.shape[0] * box.shape[1]):
+        color = "[white]"
+    else:
+        color = ""
 
-
+    cv2.putText(frame, color, (box_col_1 - 5, box_row_1 - 15), 
+            cv2.FONT_HERSHEY_SIMPLEX, 0.8, 0, 2)
+    
     cv2.imshow("resule", frame)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
-print(frame.shape[0])
-print(frame.shape[1])
-# print(frame)
-print(box)
 
+# print(frame)
+# print(box)
+# print(box.shape[0] * box.shape[1])
