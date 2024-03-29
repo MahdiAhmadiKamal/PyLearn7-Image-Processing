a9a8ccd4f886b9b8d97c75a74fa355967a7a88e7
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-03-29 22:54:55 +0430

    8th commit

--- PyLearn7-Assignment28/face_filter_video.py
+++ PyLearn7-Assignment28/face_filter_video.py
@@ -1,6 +1,7 @@
 import cv2
 import keyboard
 
+
 def mirror_filter(frame):
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     h, w = frame_gray.shape
@@ -8,16 +9,17 @@ def mirror_filter(frame):
     for i in range(h):
         for j in range(w):
             if j > w//2:
-                frame_gray[i, j] = frame_gray[i, (j-2*(j-w//2)+1)]
+                frame[i, j] = frame[i, (j-2*(j-w//2)+1)]
     
-    return frame_gray
+    return frame
+
 
 def sticker_glasses_lips(image):
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(frame_gray)
     # eye = eyes_detector.detectMultiScale(frame_gray)
     glasses_sticker = cv2.imread("input\glasses55.png")
-    zip_sticker = cv2.imread("input\zip11.png")
+    zip_sticker = cv2.imread("input\lips1.png")
     # print(eyes)
     try:
         for face in faces:
@@ -34,22 +36,27 @@ def sticker_glasses_lips(image):
                             zip_sticker[i][j] = image[yy+i, xx+j]
                         
             image[yy:yy+hh, xx:xx+ww] = zip_sticker
-           
-            if len(eye) == 2:
-                xl, yl, wl, hl = eye[0]
-               
-                xr, yr, wr, hr = eye[1]
-                
-                sticker_w = w
-                sticker_h = int(1.3*hl)
-                
-                glasses_sticker = cv2.resize(glasses_sticker, [sticker_w, sticker_h])
-                for i in range(sticker_h):
-                    for j in range(sticker_w):
-                        if glasses_sticker[i][j][0] == 0 and glasses_sticker[i][j][1] == 0 and glasses_sticker[i][j][2] == 0:
-                            glasses_sticker[i][j] = image[yl+i, x+j] 
+            # print(lips)
             
-                image[yl:yl+sticker_h, x:x+sticker_w] = glasses_sticker
+            # cv2.rectangle(image, [xx, yy], [xx + ww, yy + hh], [0, 0, 0], 4)
+        
+            xl, yl, wl, hl = eye[0]
+            # print(eye[0])
+                
+            xr, yr, wr, hr = eye[1]
+            # print(eye[1])
+            # cv2.rectangle(image, [x, y], [x+w, y+h], [0, 0, 0], 4)
+            sticker_w = w
+            sticker_h = int(1.3*hl)
+            # print(st_w)
+            # print(st_h)
+            glasses_sticker = cv2.resize(glasses_sticker, [sticker_w, sticker_h])
+            for i in range(sticker_h):
+                for j in range(sticker_w):
+                    if glasses_sticker[i][j][0] == 0 and glasses_sticker[i][j][1] == 0 and glasses_sticker[i][j][2] == 0:
+                        glasses_sticker[i][j] = image[yl+i, x+j] 
+            
+            image[yl:yl+sticker_h, x:x+sticker_w] = glasses_sticker
 
     except(IndexError):
         pass
@@ -74,7 +81,7 @@ def pixelized_face(image):
 def sticker_face(image):
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(frame_gray)
-    image_sticker = cv2.imread("input\lightbulb.png")
+    image_sticker = cv2.imread("input\mask3.png")
     for face in faces:
         x, y, w, h = face 
 
@@ -89,6 +96,7 @@ def sticker_face(image):
 
     return image
 
+
 cap = cv2.VideoCapture(0)
 face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt.xml")
 eyes_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye_tree_eyeglasses.xml")
@@ -98,17 +106,30 @@ while True:
     _, frame = cap.read()
     
 
-    if keyboard.is_pressed('3'):
-        frame3 = pixelized_face(frame)
-        cv2.imshow("result", frame3)
     if keyboard.is_pressed('1'):
         frame1 = sticker_face(frame)
         cv2.imshow("result", frame1)
     if keyboard.is_pressed('2'):
         frame2 = sticker_glasses_lips(frame)
         cv2.imshow("result", frame2)
+    if keyboard.is_pressed('3'):
+        frame3 = pixelized_face(frame)
+        cv2.imshow("result", frame3)
+    if keyboard.is_pressed('4'):
+        frame4 = mirror_filter(frame)
+        cv2.imshow("result", frame4)
 
+    cv2.putText(frame, "face sticker: 1", (10, 390), 
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, 255, 2)
+    cv2.putText(frame, "glasses and lips stickers: 2", (10, 415), 
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, 255, 2)
+    cv2.putText(frame, "pixelized face: 3", (10, 440), 
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, 255, 2)
+    cv2.putText(frame, "mirror_filter: 4", (10, 465), 
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, 255, 2)
+    
     cv2.imshow("result", frame)
+    
     
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
