fd1c69e03fc0e74caabf679040d3ed5a367e0ac1
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-04-01 23:56:47 +0430

    2nd commit

--- PyLearn7-Assignment29/1) morphing image/morphing_image.py
+++ PyLearn7-Assignment29/1) morphing image/morphing_image.py
@@ -11,3 +11,8 @@ image_brad = cv2.cvtColor(image_brad, cv2.COLOR_BGR2GRAY)
 
 image_leo = image_leo.astype(np.float32)
 image_brad = image_brad.astype(np.float32)
+
+result = (image_leo + image_brad) / 2
+result = result.astype(np.uint8)
+
+cv2.imwrite("output/result.jpg", result)
