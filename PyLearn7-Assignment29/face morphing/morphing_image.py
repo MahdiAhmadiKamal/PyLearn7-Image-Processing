01fbc64a98dd9848df4d0fefe7af7f64d697aed5
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-04-03 23:34:19 +0430

    8th commit

--- PyLearn7-Assignment29/face morphing/morphing_image.py
+++ PyLearn7-Assignment29/face morphing/morphing_image.py
@@ -1,24 +1,23 @@
-import os
-from re import X
-from turtle import xcor
+# import os
+# from re import X
+# from turtle import xcor
 import cv2
 import numpy as np
 
 
-image_leo = cv2.imread("input/ab1.jpg")
-image_brad = cv2.imread("input/ab2.jpg")
+image_start = cv2.imread("input/young.jpg")
+image_end = cv2.imread("input/old.jpg")
 
 # image_leo = cv2.cvtColor(image_leo, cv2.COLOR_BGR2GRAY)
 # image_brad = cv2.cvtColor(image_brad, cv2.COLOR_BGR2GRAY)
 
-image_leo = image_leo.astype(np.float32)
-image_brad = image_brad.astype(np.float32)
+image_end = image_end.astype(np.float32)
+image_start = image_start.astype(np.float32)
 
+step_1 = (image_start*3/4 + image_end*1/4)
+step_2 = (image_start*2/4 + image_end*2/4)
+step_3 = (image_start*1/4 + image_end*3/4)
 
-step_1 = (image_leo*3/4 + image_brad*1/4)
-step_2 = (image_leo*2/4 + image_brad*2/4)
-step_3 = (image_leo*1/4 + image_brad*3/4)
-
-result = np.concatenate((image_brad, step_3, step_2, step_1, image_leo), axis=1)
+result = np.concatenate((image_start, step_1, step_2, step_3, image_end), axis=1)
 result = result.astype(np.uint8)
-cv2.imwrite("output/result.jpg", result)
+cv2.imwrite("output/result2.jpg", result)
---
+++ PyLearn7-Assignment29/face morphing/readme.md
@@ -0,0 +1,22 @@
+# Face Morphing
+Using opencv in python, this program can morph one face into another.
+
+## How to Install
+Run following command:
+```
+pip install -r requirements.txt
+```
+
+## How to Run
+execute this command in terminal:
+```
+python morphing_image.py
+```
+
+## Result
+![output](output/result2.jpg)
+
+## Python
+This program is written using [Python](https://www.python.org/) language and [OpenCV](https://opencv.org/), whick is a library of programming functions for real-time computer vision, image processing, and machine learning.
+
+<img src="input\opencv.webp" width="300" height="142.57">
