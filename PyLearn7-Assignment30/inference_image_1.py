e283f1d78c619ccd538406263f11a4530955f882
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-04-08 20:56:53 +0430

    3rd commit

--- PyLearn7-Assignment30/inference_image_1.py
+++ PyLearn7-Assignment30/inference_image_1.py
@@ -20,10 +20,21 @@ boxes, scores = fd.inference(image)
 for pred in fa.get_landmarks(image, boxes):
     for i, p in enumerate(np.round(pred).astype(np.int32)):
         cv2.circle(image, tuple(p), 2, color, -1, cv2.LINE_AA)
-        cv2.putText(image, str(i), tuple(p), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0, 0, 255))
+        cv2.putText(image, str(i), tuple(p), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0, 0, 0))
+
+    lips_landmarks = []
+    for i in [52, 55, 56, 53, 59, 58, 61, 68, 67, 71, 63, 64]:
+        lips_landmarks.append(pred[i])
+    lips_landmarks = np.array(lips_landmarks, dtype=int)
+
+    print("* * *")
+    print(lips_landmarks)
+    print("* * *")
+
 
 print(time.perf_counter() - start_time)
 
 cv2.imshow("result", image)
 cv2.waitKey()
 cv2.imwrite("output/result1.jpg", image)
+
