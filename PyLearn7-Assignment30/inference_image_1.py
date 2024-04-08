7f90b5c64c7748c79b16e331dc65a65616550100
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-04-09 00:02:12 +0430

    4th commit

--- PyLearn7-Assignment30/inference_image_1.py
+++ PyLearn7-Assignment30/inference_image_1.py
@@ -7,6 +7,10 @@ from TFLiteFaceDetector import UltraLightFaceDetecion
 from TFLiteFaceAlignment import CoordinateAlignmentModel
 
 
+def zoom_effect (landmark):
+    ...
+    return result
+
 fd = UltraLightFaceDetecion("weights/RFB-320.tflite", conf_threshold=0.88)
 fa = CoordinateAlignmentModel("weights/coor_2d106.tflite")
 
@@ -18,9 +22,9 @@ start_time = time.perf_counter()
 boxes, scores = fd.inference(image)
 
 for pred in fa.get_landmarks(image, boxes):
-    for i, p in enumerate(np.round(pred).astype(np.int32)):
-        cv2.circle(image, tuple(p), 2, color, -1, cv2.LINE_AA)
-        cv2.putText(image, str(i), tuple(p), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0, 0, 0))
+    # for i, p in enumerate(np.round(pred).astype(np.int32)):
+    #     cv2.circle(image, tuple(p), 2, color, -1, cv2.LINE_AA)
+    #     cv2.putText(image, str(i), tuple(p), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0, 0, 0))
 
     lips_landmarks = []
     for i in [52, 55, 56, 53, 59, 58, 61, 68, 67, 71, 63, 64]:
@@ -31,10 +35,19 @@ for pred in fa.get_landmarks(image, boxes):
     print(lips_landmarks)
     print("* * *")
 
+    x, y, w, h = cv2.boundingRect(lips_landmarks)
+    mask = np.zeros(image.shape, dtype=np.uint8)
+    cv2.drawContours(mask, [lips_landmarks], -1, (255, 255, 255), -1)
+    mask = mask // 255
+
+    result = image * mask
+    result = result [y:y+h, x:x+w]
+
+    result_big = cv2.resize(result, (0, 0), fx=4, fy=4)
 
 print(time.perf_counter() - start_time)
 
-cv2.imshow("result", image)
+cv2.imshow("result", result_big)
 cv2.waitKey()
-cv2.imwrite("output/result1.jpg", image)
+cv2.imwrite("output/result1.jpg", result_big)
 
