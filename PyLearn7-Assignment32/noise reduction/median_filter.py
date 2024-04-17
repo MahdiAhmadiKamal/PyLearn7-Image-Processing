7a9bc1a62897e419a7e0d1c8b4d6c6e6a9cc9ca4
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-04-17 20:05:05 +0430

    1st commit

---
+++ PyLearn7-Assignment32/noise reduction/median_filter.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+
+image = cv2.imread("output/result.jpg", cv2.IMREAD_GRAYSCALE)
+# result = np.zeros(image.shape)
+
+# for i in range(1, image.shape[0]-1):
+#     for j in range(1, image.shape[1]-1):
+#         region = image[i-1: i+2, j-1: j+2]
+#         sorted_array = np.sort(region, axis=None)
+#         result[i, j] = sorted_array[4]
+
+result = cv2.medianBlur(image, 3)
+
+cv2.imwrite("output/result22.jpg", result)
+
