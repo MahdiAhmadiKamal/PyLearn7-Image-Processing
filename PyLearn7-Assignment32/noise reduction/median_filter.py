283a6959e3c4843aa6132a553c9c20b84f0e9d2e
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-04-19 19:44:12 +0430

    5th commit

--- PyLearn7-Assignment32/noise reduction/median_filter.py
+++ PyLearn7-Assignment32/noise reduction/median_filter.py
@@ -2,16 +2,43 @@ import cv2
 import numpy as np
 
 
-image = cv2.imread("input/xray_noisy.png", cv2.IMREAD_GRAYSCALE)
-# result = np.zeros(image.shape)
+image1=cv2.imread("Input/xray_noisy.png", cv2.IMREAD_GRAYSCALE)
+image2=cv2.imread("Input/board.tif", cv2.IMREAD_GRAYSCALE)
+image3=cv2.imread("Input/image.tif", cv2.IMREAD_GRAYSCALE)
+image4=cv2.imread("Input/balloons.png")
+image5=cv2.imread("Input/face.png")
+image6=cv2.imread("Input/fig.tif", cv2.IMREAD_GRAYSCALE)
 
-# for i in range(1, image.shape[0]-1):
-#     for j in range(1, image.shape[1]-1):
-#         region = image[i-1: i+2, j-1: j+2]
-#         sorted_array = np.sort(region, axis=None)
-#         result[i, j] = sorted_array[4]
+result1 = cv2.medianBlur(image1, 3)
 
-result = cv2.medianBlur(image, 3)
+result2 = cv2.medianBlur(image2, 3)
+result2 = cv2.medianBlur(result2, 3)
 
-cv2.imwrite("output/result.jpg", result)
+result3 = cv2.medianBlur(image3, 3)
+result3 = cv2.medianBlur(result3, 3)
 
+result4 = cv2.medianBlur(image4, 3)
+result4 = cv2.medianBlur(result4, 3)
+result4 = cv2.medianBlur(result4, 3)
+
+result5 = cv2.medianBlur(image5, 3)
+result5 = cv2.medianBlur(result5, 3)
+result5 = cv2.medianBlur(result5, 3)
+result5 = cv2.medianBlur(result5, 3)
+
+result6 = cv2.medianBlur(image6, 3)
+result6 = cv2.medianBlur(result6, 3)
+
+result1 = np.hstack((image1, result1))
+result2 = np.hstack((image2, result2))
+result3 = np.hstack((image3, result3))
+result4 = np.hstack((image4, result4))
+result5 = np.hstack((image5, result5))
+result6 = np.hstack((image6, result6))
+
+cv2.imwrite("output/result1.jpg", result1)
+cv2.imwrite("output/result22.jpg", result2)
+cv2.imwrite("output/result33.jpg", result3)
+cv2.imwrite("output/result444.jpg", result4)
+cv2.imwrite("output/result5555.jpg", result5)
+cv2.imwrite("output/result66.jpg", result6)
---
+++ PyLearn7-Assignment32/noise reduction/readme.md
@@ -0,0 +1,42 @@
+
+# Noise Reduction Using Median Filter ✨
+**Median filter** is applied on some noisy images (for one or more times) to reduce their noise.
+
+## How to Install
+Run following command:
+```
+pip install -r requirements.txt
+```
+
+## How to Run
+Execute this command in terminal:
+```
+python median_filter.py
+```
+
+## Results
+Left images are **inputs** and right ones are **output** images after applying **median filter**.
+
+### 1. kernel 5x5: 0.04
+<img src="output\result1.jpg" width="512" height="256">
+
+### 2. kernel 5x5: 1.0
+<img src="output\result22.jpg" width="512" height="247.6">
+
+### 3. kernel 5x5: 5.0
+<img src="output\result33.jpg" width="512" height="256">
+
+### 4. kernel 3x3: 0.04
+<img src="output\result444.jpg" width="512" height="192">
+
+### 5. kernel 3x3: 1.0
+<img src="output\result5555.jpg" width="418" height="218">
+
+### 6. kernel 3x3: 5.0
+<img src="output\result66.jpg" width="512" height="256">
+
+
+## Python
+This program is written using [Python](https://www.python.org/) language and [OpenCV](https://opencv.org/), whick is a library of programming functions for real-time computer vision, image processing, and machine learning.
+
+<img src="input\opencv.webp" width="262.5" height="124.75">
