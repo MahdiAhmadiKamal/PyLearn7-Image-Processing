6aa8903e0fa1591b41b60b539d45ef37d925702c
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-04-28 13:02:07 +0430

    2nd commit

--- PyLearn7-Assignment34/color image to grayscale image/color_to_grayscale.py
+++ PyLearn7-Assignment34/color image to grayscale image/color_to_grayscale.py
@@ -1,18 +1,24 @@
 import cv2
 import numpy as np
 
+def color2gray(image):
+    
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    rows, cols, _ = image.shape
+
+    R, G, B = cv2.split(image)
+
+    result = np.zeros((rows, cols), dtype=np.uint8)
+
+    for row in range(rows):
+        for col in range(cols):
+            result[row, col] = R[row, col]//3 + G[row, col]//3 + B[row, col]//3
+
+    cv2.imshow("", result)
+    cv2.waitKey(0)
+    return result
+
 
 image = cv2.imread("input/macaw.jpg")
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-rows, cols, _ = image.shape
-
-R, G, B = cv2.split(image)
-
-result = np.zeros((rows, cols), dtype=np.uint8)
-
-for row in range(rows):
-    for col in range(cols):
-        result[row, col] = R[row, col]//3 + G[row, col]//3 + B[row, col]//3
-
-cv2.imshow("", result)
-cv2.waitKey(0)
+gray_image = color2gray(image)
+cv2.imwrite("output/gray image.jpg", gray_image)
