beec0f453713911974892043cddb75c6daa93614
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-05-04 10:51:55 +0430

    readme added

--- PyLearn7-Assignment35/color detector/color_detector.py
+++ PyLearn7-Assignment35/color detector/color_detector.py
@@ -1,4 +1,3 @@
-import fractions
 import cv2
 import numpy as np
 
@@ -8,7 +7,6 @@ box = np.zeros((110, 110), dtype=int)
 
 while True:
     _, frame = cap.read()
-    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     
     box_col_1 = frame.shape[1]//2 - 55
     box_row_1 = frame.shape[0]//2 - 55
@@ -18,24 +16,18 @@ while True:
     cv2.rectangle(frame, (box_col_1, box_row_1), (box_col_2, box_row_2), 0, 7)
     box = frame[box_col_1:box_col_2, box_row_1:box_row_2]
 
-    # b, g, r = cv2.split(frame)
-
     blue = np.count_nonzero(box[:, :, 0] > 120)
     green = np.count_nonzero(box[:, :, 1] > 140)
     red = np.count_nonzero(box[:, :, 2] > 130)
 
-    # b = np.count_nonzero((box < 80))
-    # g = np.count_nonzero((box > 120) & (box < 160))
-    # w = np.count_nonzero((box > 150))
-    
-    if red > 0.45 * (box.shape[0] * box.shape[1]) and green > 0.45 * (box.shape[0] * box.shape[1]):
+    if blue > 0.33 * (box.shape[0] * box.shape[1]) and green > 0.33 * (box.shape[0] * box.shape[1]) and red > 0.33 * (box.shape[0] * box.shape[1]):
+        color = "[white]" 
+    elif red > 0.45 * (box.shape[0] * box.shape[1]) and green > 0.45 * (box.shape[0] * box.shape[1]):
         color = "[yellow]"
     elif red > 0.6 * (box.shape[0] * box.shape[1]) and green > 0.3 * (box.shape[0] * box.shape[1]):
         color = "[orange]"
     elif red > 0.37 * (box.shape[0] * box.shape[1]) and blue > 0.53 * (box.shape[0] * box.shape[1]):
         color = "[purple]"
-    elif blue > 0.33 * (box.shape[0] * box.shape[1]) and green > 0.33 * (box.shape[0] * box.shape[1]) and red > 0.33 * (box.shape[0] * box.shape[1]):
-        color = "[white]" 
     elif blue < 0.03 * (box.shape[0] * box.shape[1]) and green < 0.03 * (box.shape[0] * box.shape[1]) and red < 0.03 * (box.shape[0] * box.shape[1]):
         color = "[black]" 
     elif blue > 0.9 * (box.shape[0] * box.shape[1]):
@@ -50,11 +42,6 @@ while True:
     cv2.putText(frame, color, (box_col_1 - 5, box_row_1 - 15), 
             cv2.FONT_HERSHEY_SIMPLEX, 0.8, 0, 2)
     
-    cv2.imshow("B&W Color Detector", frame)
+    cv2.imshow("Color Detector", frame)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
-
-
-# print(frame)
-# print(box)
-# print(box.shape[0] * box.shape[1])
---
+++ PyLearn7-Assignment35/color detector/readme.md
@@ -0,0 +1,37 @@
+# Color Detector 🔴🟢🔵
+Using OpenCV techniques in Python, **Color Detector** detects different colors.
+
+## How to Install
+Run following command:
+```
+pip install -r requirements.txt
+```
+
+## How to Run
+Execute this command in terminal:
+```
+python color_detector.py
+```
+
+## Results
+
+<img src="pics\black.png" width="500" height="400">
+
+<img src="pics\blue.png" width="500" height="400">
+
+<img src="pics\green.png" width="500" height="400">
+
+<img src="pics\orange.png" width="500" height="400">
+
+<img src="pics\purple.png" width="500" height="400">
+
+<img src="pics\red.png" width="500" height="400">
+
+<img src="pics\white.png" width="500" height="400">
+
+<img src="pics\yellow.png" width="500" height="400">
+
+## Python
+This program is written using [Python](https://www.python.org/) language and [OpenCV](https://opencv.org/), whick is a library of programming functions for real-time computer vision, image processing, and machine learning.
+
+<img src="opencv.webp" width="262.5" height="124.75">
